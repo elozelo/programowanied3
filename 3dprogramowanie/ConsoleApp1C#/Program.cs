4572eac9b0e54db056b481184e385ea2af8d6c79
elozelo <144440942+elozelo@users.noreply.github.com>
2024-09-10 16:03:39 +0200

    Add files via upload

---
+++ 3dprogramowanie/ConsoleApp1C#/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Hello, World!");
+Console.Write("Hello, World!\n");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+
+int x = 10;
+string name = "Jan";
+string surname = "Kowalski";
+
+Console.WriteLine(x);
+Console.WriteLine(name);
+Console.WriteLine("Witaj " + name + " " + surname + " tutaj");
+Console.WriteLine("Witaj {0} {1} tutaj", name, surname);
+
+Console.WriteLine($"Witaj {name} {surname} tutaj");
+
+Console.WriteLine("Predkosc to km\\h");
+Console.WriteLine(@"Predkosc to km\h");
+Console.WriteLine($@"Predkosc {name} to km\h");
+
+int number = 5;
+int secondNumber = number;
+secondNumber = 6;
+Console.WriteLine($"number = {number}");
+Console.WriteLine($"number = {secondNumber}");
+
+
+
+void ParametrTest_v1(int p)
+{
+    
+    p++;
+    Console.WriteLine($"ParametrTest_v1: p = {p}");
+}
+void ParametrTest_v2(ref int p)
+{
+    
+    p++;
+    Console.WriteLine($"ParametrTest_v2: p = {p}");
+}
+void ParametrTest_v3(out int p)
+{
+    
+    p++;
+    Console.WriteLine($"ParametrTest_v3: p = {p}");
+}
+
+ParametrTest_v1(number);
+Console.WriteLine($"number = {number}");
+
+ParametrTest_v2(ref number);
+Console.WriteLine($"number parametr test 2 = {number}");
+
+int thirdNumber;
+ParametrTest_v3(out number);
+Console.WriteLine($"number = {thirdNumber}");
